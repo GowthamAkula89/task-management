7574ec1713e534d7a47a420378424c7973c3a620
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-10-25 16:08:43 +0530

    udpate taskModel

--- server/src/models/taskModel.js
+++ server/src/models/taskModel.js
@@ -12,7 +12,7 @@ const taskSchema = new mongoose.Schema({
     },
     status : {
         type: String,
-        enum : ['Pending', 'In progress', 'Completed'],
+        enum : ['Pending', 'In Progress', 'Completed'],
         default :'Pending'
     }
 })
